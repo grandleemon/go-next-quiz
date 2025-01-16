55e43adbb5adcd4d1fd7b8d048e25c7974822332
Maxim <zeetreex@gmail.com>
2025-01-16 22:48:07 +0200

    fix cors

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetch("http://localhost:8080/ping");
+  const data = await response.json();
+
+  console.log("data", data)
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        w
         <Image
-          className="dark:invert"
+          className="dark"
           src="/next.svg"
           alt="Next.js logo"
           width={180}
--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+
+	cors "github.com/rs/cors/wrapper/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	r.Use(cors.Default())
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
